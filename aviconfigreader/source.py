5ce08240628b764bb4f50def0e1035e81452cfcd
Avinash <avinash.kumar@algoscale.com>
2022-11-30 15:32:43 +0530

    .

--- aviconfigreader/source.py
+++ aviconfigreader/source.py
@@ -1,6 +1,7 @@
 import os
 import yaml
 import configparser
+import json
 
 def readconfig(input_file, output_filename, output_file_extension):
     """
@@ -35,10 +36,10 @@ def readconfig(input_file, output_filename, output_file_extension):
                     for key, value in config.items():
                         output_file.write(f"{key}={value}")
         else:
-            for key, value in config.items():
+            for key, value in data_loaded.items():
                 os.environ.setdefault[key] = value
     # get file extension of input file yaml/cfg/conf
-    filename, extension = os.path.splitext('input_file')
+    filename, extension = os.path.splitext(input_file)
     if extension == ".yaml":
         read_yaml_file(input_file, output_filename, output_file_extension)
     elif extension == ".conf" or "cfg":
