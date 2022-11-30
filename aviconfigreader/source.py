c6066fc5418668e25bcde9eba22c9aaa694d99a6
Avinash <avinash.kumar@algoscale.com>
2022-11-30 17:40:07 +0530

    updated code

--- aviconfigreader/source.py
+++ aviconfigreader/source.py
@@ -25,18 +25,22 @@ def readconfig(input_file, output_filename, output_file_extension):
             for key, value in data_loaded.items():
                 os.environ.setdefault[key] = value
     def read_conf_file(input_file, output_filename, output_file_extension):
-        config = configparser.RawConfigParser()
+        config = configparser.ConfigParser()
         config.read(input_file)
+        flat_dict = {}
+        for sect in config.sections():
+            for k,v in config.items(sect):
+                flat_dict[k] = v
         if output_file_extension == ".env" or ".json":
             output_path = output_filename+output_file_extension
             with open(output_path, 'w') as output_file:
                 if output_file_extension == ".json":
-                    json.dump(config.items(), output_file)
+                    json.dump(flat_dict, output_file)
                 elif output_file_extension == ".env":
-                    for key, value in config.items():
+                    for key, value in flat_dict.items():
                         output_file.write(f"{key}={value}")
         else:
-            for key, value in config.items():
+            for key, value in flat_dict.items():
                 os.environ.setdefault[key] = value
     # get file extension of input file yaml/cfg/conf
     filename, extension = os.path.splitext(input_file)
@@ -46,3 +50,5 @@ def readconfig(input_file, output_filename, output_file_extension):
         read_conf_file(input_file, output_filename, output_file_extension)
     else:
         raise IOError("invalid input file format")
+
+# readconfig("sample.cfg", "outfile", ".json")
